1511a47f147377fcd982a192322aab95766ee460
anil pothuri <pothurianil.naidu@extwebtech.in>
2022-12-01 12:29:55 +0530

    search products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -8,4 +8,16 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test 'should filter produts by title' do
+    assert_equal 1, Product.filter_by_title('first').count
+  end
+
+  test 'should filter by title and sort them' do
+    assert_equal [products(:three)], Product.filter_by_title('first').sort
+  end
+
+  test 'should filter by price and sort them' do
+    assert_equal [products(:three), products(:four)], Product.above_the_price(400).sort
+  end
 end
